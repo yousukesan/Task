9c46372d4c1cbfd9b3060892fd75cb5b889b8485
y.k <kishinoyousuke@mail.goo.ne.jp>
2018-08-30 14:19:28 +0000

    CRUD messages

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,58 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'task が正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'task が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(message_params)
+      flash[:success] = 'Message は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Message は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'Message は正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+   private
+
+  # Strong Parameter
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
+
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'toppages#index'
+  root to: 'tasks#index'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  get 'signup', to: 'users#new'
+  get 'signup', to: 'tasks#index'
   resources :users, only: [:index, :show, :new, :create]
+  
+  resources :tasks
 end
